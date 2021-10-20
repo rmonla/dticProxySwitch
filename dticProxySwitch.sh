2c108532cf74bd3926c175a25bae2748749fea03
Ricardo MONLA <rmonla@gmail.com>
2021-10-20 12:06:02 -0300

    v2.5 => FXs: cargaInterface() Optimiza código con parámetro BODY.

--- dticProxySwitch.sh
+++ dticProxySwitch.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 clear
 
-# |||||||||||||||||||| VARIABLES ||||||||||||||||||||
+######## «® VARIABLES ®» ########
 
 
 
-# |||||||||||||||||||| FUNCIONES ||||||||||||||||||||
-getStrRED(){
-	local RED ETH IP
-	RED="$1"; ETH="$2"; IP="$3"
+######## «® FUNCIONES ®» ########
+getStrGTWAY(){
+	local RED IP
+	RED="$1"; IP="$2"; 
     
     echo  "
-# «® RED $RED ®»
+######## «® GATEWAY ®» ########
+gateway $RED.$IP
+# «® ------------------ ®»
 
+"
+
+    # echo -n "$RED" "$ETH" "$IP" >&2
+}
+getStrRED(){
+	local ETH RED IP
+	ETH="$1"; RED="$2"; IP="$3"
+    
+    echo  "
+######## «® RED $RED ®» ########
 allow-hotplug eth$ETH
 iface eth$ETH inet static
 
@@ -21,38 +33,37 @@ netmask 255.255.255.0
 broadcast $RED.255
 network $RED.0
 # «® ------------------ ®»
+
 "
 
     # echo -n "$RED" "$ETH" "$IP" >&2
 }
 
 cargaInterface () {
+	local BODY
+	BODY="$1";
 
 	cat > interfaces.conf << _INTERFACE
-# «® ------------------ ®»
-
+######## «® LOOPBACK ®» ########
 source /etc/network/interfaces.d/*
 auto lo
 iface lo inet loopback
 # «® ------------------ ®»
-$red_8
-$red_7
-$red_9
-
+$BODY
 _INTERFACE
 
 }
 
 
 
-# |||||||||||||||||||| MENU ||||||||||||||||||||
+######## «® MENU ®» ########
 
 while :
 do
 echo "«® ELEGIR EL TRONCAL DE INTERNET PARA EL PROXY ®»";
 echo "";
-echo "1. WAN IPT 30Mb Asincrónico.";
-echo "2. WAN IPT 10Mb Asincrónico.";
+echo "1. WAN IPT 30Mb A-sincrónico.";
+echo "2. WAN IPT 10Mb A-sincrónico.";
 echo "3. WAN TECO 60Mb Sincrónico.";
 echo "";
 echo "4. SALIR";
@@ -63,49 +74,58 @@ echo "";
 read opt
 case $opt in
 
-1) # |||||||||||||||||||| OPCION 1 ||||||||||||||||||||
+1) ######## «® OPCION 1 ®» ########
 clear;
-echo "";
 
-red_8=$(getStrRED '192.168.8' 0 9)
-red_7=$(getStrRED '192.168.7' 1 1)
-red_9=$(getStrRED '192.168.9' 1 5)
+cargaInterface "
+$(getStrRED 0 '192.168.8'  9)
+$(getStrGTWAY '192.168.8' 86)
+$(getStrRED 1 '192.168.7'  1)
+$(getStrRED 2 '192.168.20' 1)
+" 
 
-cargaInterface
-
-echo "####### END OPCION 1 #######";
+echo "--- END OPCION 1 ---";
 echo "";;
 
-2) # |||||||||||||||||||| OPCION 2 ||||||||||||||||||||
+2) ######## «® OPCION 2 ®» ########
 clear;
-echo "";
 
+cargaInterface " 
+$(getStrRED 0 '192.168.11' 4)
+$(getStrGTWAY '192.168.11' 1)
+$(getStrRED 1 '192.168.7'  1)
+$(getStrRED 2 '192.168.20' 1)
+" 
 
-echo "#######  END OPCION 2 #######";
+echo "--- END OPCION 2 ---";
 echo "";;
 
-3) # |||||||||||||||||||| OPCION 3 ||||||||||||||||||||
+3) ######## «® OPCION 3 ®» ########
 clear;
-echo "";
 
+cargaInterface " 
+$(getStrRED 0 '190.114.205' 30)
+$(getStrGTWAY '190.114.205' 1)
+$(getStrRED 1 '192.168.7'  1)
+$(getStrRED 2 '192.168.20' 1)
+" 
 
-echo "####### END OPCION 3 #######";
+echo "--- END OPCION 3 ---";
 echo "";;
 
-4) # |||||||||||||||||||| OPCION 4 ||||||||||||||||||||
+4) ######## «® OPCION 4 ®» ########
 clear;
-echo "";
 
 
 
-echo "####### END OPCION 4 #######";
+echo "--- END OPCION 4 ---";
 echo "";
 echo "";
 exit 1;;
 
-*) # |||||||||||||||||||| OPCION OUT ||||||||||||||||||||
+*) ######## «® OPCION OUT ®» ########
 
-echo "$opc no es una opcion válida.";
+echo "$opc no es una opción válida.";
 echo "Presiona una tecla para continuar...";
 read foo;;
 esac
