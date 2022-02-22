9c57db5d810b7acff70671e5b80300f77a8b667d
Ricardo MONLA <rmonla@gmail.com>
2022-02-22 03:05:14 -0300

    Add files via upload

--- dticProxySwitch.sh
+++ dticProxySwitch.sh
@@ -240,10 +240,11 @@ salePor8_89(){
 
 	cargaInterface "
 	$(getStrRED 0 '192.168.8'  9)
+	$(getStrGTWAY '192.168.8' 89)
+	
 	$(getStrRED 1 '192.168.7'  1)
 	$(getStrRED 2 '10.0.10' 1)
 
-	$(getStrGTWAY '192.168.8' 89)
 	
 	" 
 	ejecutarRUTEOs
@@ -253,10 +254,11 @@ salePor8_87(){
 
 	cargaInterface "
 	$(getStrRED 0 '192.168.8'  9)
+	$(getStrGTWAY '192.168.8' 87)
+
 	$(getStrRED 1 '192.168.7'  1)
 	$(getStrRED 2 '10.0.10' 1)
 	
-	$(getStrGTWAY '192.168.8' 87)
 	" 
 	ejecutarRUTEOs
 }
